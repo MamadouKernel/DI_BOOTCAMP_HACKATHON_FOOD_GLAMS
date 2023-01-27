01ac9fb94adf583f98ec99b02b3d9c341002903f
Ayou Lasme <ayou.lasme@mail.com>
2023-01-27 14:29:07 +0000

    Stockage des elements dans le localeStorage lorsque le bouton est cliqué

--- assets/script/script.js
+++ assets/script/script.js
@@ -13,60 +13,61 @@ let divCardAfricain = document.querySelector(".divCardAfricain");
 const platsAfricain = [
     {
         image: "assets/imgs/africain/im3.jpg",
-        nom : "Escargot sauté",
-        prix : "2000 Fcfa",
+        nom: "Escargot sauté",
+        prix: "2000 Fcfa",
     },
 
     {
         image: "assets/imgs/africain/im5.jpg",
-        nom : "Riz Patte Feuille",
-        prix : "7000 Fcfa",
+        nom: "Riz Patte Feuille",
+        prix: "7000 Fcfa",
     },
 
     {
         image: "assets/imgs/africain/im6.jpg",
-        nom : "Sauce Arrachide",
-        prix : "8000 Fcfa",
+        nom: "Sauce Arrachide",
+        prix: "8000 Fcfa",
     },
 
     {
         image: "assets/imgs/africain/im8.jpg",
-        nom : "Soupe de mouton",
-        prix : "7000 Fcfa",
+        nom: "Soupe de mouton",
+        prix: "7000 Fcfa",
     },
 
     {
         image: "assets/imgs/africain/im9.jpg",
-        nom : "Soupe du Pêcheur",
-        prix : "10.000 Fcfa",
+        nom: "Soupe du Pêcheur",
+        prix: "10.000 Fcfa",
     },
 
     {
         image: "assets/imgs/africain/im10.jpg",
-        nom : "Sauce Graine",
-        prix : "8000 Fcfa",
+        nom: "Sauce Graine",
+        prix: "8000 Fcfa",
     },
 
     {
         image: "assets/imgs/africain/im11.jpg",
-        nom : "Garba",
-        prix : "3000 Fcfa",
+        nom: "Garba",
+        prix: "3000 Fcfa",
     },
 ]
 
 
 
-for (const plat of platsAfricain){
+for (const plat of platsAfricain) {
     let div1 = document.createElement("div");
     div1.classList.add("col-3");
     divCardAfricain.appendChild(div1);
 
     let div = document.createElement("div");
     div.classList.add("card");
+    div.dataset.produit = JSON.stringify(plat);
     div1.appendChild(div);
 
     let img = document.createElement("img")
-    img.classList.add("imageCard");         
+    img.classList.add("imageCard");
     img.src = plat.image;
     div.appendChild(img);
 
@@ -84,6 +85,19 @@ for (const plat of platsAfricain){
     ButtonPlat.classList.add("buttonPlat");
     div.appendChild(ButtonPlat);
     ButtonPlat.textContent = "Ajouter Au Panier"
+    ButtonPlat.onclick = function (e) {
+        let card = ButtonPlat.closest('.card')
+        console.log(card.dataset.produit);
+        // ajouter le produit au panier
+        let panier = localStorage.getItem('panier')
+        if (panier) 
+            panier = JSON.parse(panier)
+        else 
+            panier = {}
+        panier.produits = panier.hasOwnProperty('produits') ? panier.produits : [];
+        panier.produits.push({...JSON.parse(card.dataset.produit), quantite: 1})
+        localStorage.setItem("panier", JSON.stringify(panier))
+    }
 }
 
 
@@ -100,50 +114,50 @@ let divCardAmericain = document.querySelector(".divCardAmericain");
 const platsAmericain = [
     {
         image: "assets/imgs/americain/Cauliflower.jpg",
-        nom : "Cauliflower",
-        prix : "7000 Fcfa",
+        nom: "Cauliflower",
+        prix: "7000 Fcfa",
     },
 
     {
         image: "assets/imgs/americain/easy_all_americaine.jpg",
-        nom : "Easy all ameriaine",
-        prix : "8000 Fcfa",
+        nom: "Easy all ameriaine",
+        prix: "8000 Fcfa",
     },
 
     {
         image: "assets/imgs/americain/quesadillas_au_poulet.jpg",
-        nom : "Quesadillas au poulet",
-        prix : "8000 Fcfa",
+        nom: "Quesadillas au poulet",
+        prix: "8000 Fcfa",
     },
 
     {
         image: "assets/imgs/americain/roasted_chilli.jpg",
-        nom : "Soupe de mouton",
-        prix : "7000 Fcfa",
+        nom: "Soupe de mouton",
+        prix: "7000 Fcfa",
     },
 
     {
         image: "assets/imgs/americain/Salade à l'americaine.jpg",
-        nom : "Salade à l'americaine",
-        prix : "5000 Fcfa",
+        nom: "Salade à l'americaine",
+        prix: "5000 Fcfa",
     },
 
     {
         image: "assets/imgs/americain/super_bowl.jpg",
-        nom : "Super bowl",
-        prix : "8000 Fcfa",
+        nom: "Super bowl",
+        prix: "8000 Fcfa",
     },
 
     {
         image: "assets/imgs/americain/Travers_de_porc.jpg",
-        nom : "Travers de porc",
-        prix : "10.000 Fcfa",
+        nom: "Travers de porc",
+        prix: "10.000 Fcfa",
     },
 ]
 
 
 
-for (const plat of platsAmericain){
+for (const plat of platsAmericain) {
     let div1 = document.createElement("div");
     div1.classList.add("col-3");
     divCardAmericain.appendChild(div1);
@@ -153,7 +167,7 @@ for (const plat of platsAmericain){
     div1.appendChild(div);
 
     let img = document.createElement("img")
-    img.classList.add("imageCard");         
+    img.classList.add("imageCard");
     img.src = plat.image;
     div.appendChild(img);
 
@@ -187,50 +201,50 @@ let divCardAsiatique = document.querySelector(".divCardAsiatique");
 const platsAsiatique = [
     {
         image: "assets/imgs/Asiatique/Boeuf_lok_lak.jpg",
-        nom : "Boeuf lok lak",
-        prix : "7000 Fcfa",
+        nom: "Boeuf lok lak",
+        prix: "7000 Fcfa",
     },
 
     {
         image: "assets/imgs/Asiatique/recette_de_porc_japonnaise.jpg",
-        nom : "Recette de porc japonnaise",
-        prix : "8000 Fcfa",
+        nom: "Recette de porc japonnaise",
+        prix: "8000 Fcfa",
     },
 
     {
         image: "assets/imgs/Asiatique/rice_noodle_soup.jpg",
-        nom : "Rice noodle soup",
-        prix : "8000 Fcfa",
+        nom: "Rice noodle soup",
+        prix: "8000 Fcfa",
     },
 
     {
         image: "assets/imgs/Asiatique/nems.jpg",
-        nom : "Nems",
-        prix : "7000 Fcfa",
+        nom: "Nems",
+        prix: "7000 Fcfa",
     },
 
     {
         image: "assets/imgs/Asiatique/sczechuan.jpg",
-        nom : "Sczechuan",
-        prix : "5000 Fcfa",
+        nom: "Sczechuan",
+        prix: "5000 Fcfa",
     },
 
     {
         image: "assets/imgs/Asiatique/shrimp.jpg",
-        nom : "Shrimp",
-        prix : "8000 Fcfa",
+        nom: "Shrimp",
+        prix: "8000 Fcfa",
     },
 
     {
         image: "assets/imgs/Asiatique/torikatsu.jpg",
-        nom : "torikatsu",
-        prix : "10.000 Fcfa",
+        nom: "torikatsu",
+        prix: "10.000 Fcfa",
     },
 ]
 
 
 
-for (const plat of platsAsiatique){
+for (const plat of platsAsiatique) {
     let div1 = document.createElement("div");
     div1.classList.add("col-3");
     divCardAsiatique.appendChild(div1);
@@ -240,7 +254,7 @@ for (const plat of platsAsiatique){
     div1.appendChild(div);
 
     let img = document.createElement("img")
-    img.classList.add("imageCard");         
+    img.classList.add("imageCard");
     img.src = plat.image;
     div.appendChild(img);
 
@@ -273,62 +287,62 @@ let divCardEuropeen = document.querySelector(".divCardEuropeen");
 const platsEuropeen = [
     {
         image: "assets/imgs/Europeen/Escalope_de_veau_milannaise.jpg",
-        nom : "Escalope de veau milannaise",
-        prix : "7000 Fcfa",
+        nom: "Escalope de veau milannaise",
+        prix: "7000 Fcfa",
     },
 
     {
         image: "assets/imgs/Europeen/German_Pot.jpg",
-        nom : "German Pot",
-        prix : "8000 Fcfa",
+        nom: "German Pot",
+        prix: "8000 Fcfa",
     },
 
     {
         image: "assets/imgs/Europeen/Haluski.jpg",
-        nom : "Haluski",
-        prix : "8000 Fcfa",
+        nom: "Haluski",
+        prix: "8000 Fcfa",
     },
 
     {
         image: "assets/imgs/Europeen/potato_Round.jpg",
-        nom : "Potato Round",
-        prix : "7000 Fcfa",
+        nom: "Potato Round",
+        prix: "7000 Fcfa",
     },
 
     {
         image: "assets/imgs/Europeen/poulet_aux_noix_de_cajoux.jpg",
-        nom : "poulet aux noix de cajoux",
-        prix : "12.000 Fcfa",
+        nom: "poulet aux noix de cajoux",
+        prix: "12.000 Fcfa",
     },
 
     {
         image: "assets/imgs/Europeen/Risoto_Royal.jpg",
-        nom : "Risoto_Royal",
-        prix : "8000 Fcfa",
+        nom: "Risoto_Royal",
+        prix: "8000 Fcfa",
     },
 
     {
         image: "assets/imgs/Europeen/Roti_De_Porc_aux_Pommes_et_pruneaux.jpg",
-        nom : "Roti De Porc aux Pommes et pruneaux",
-        prix : "10.000 Fcfa",
+        nom: "Roti De Porc aux Pommes et pruneaux",
+        prix: "10.000 Fcfa",
     },
 
     {
         image: "assets/imgs/Europeen/Roti_De_Porc.jpg",
-        nom : "Roti De Porc",
-        prix : "10.000 Fcfa",
+        nom: "Roti De Porc",
+        prix: "10.000 Fcfa",
     },
 
     {
         image: "assets/imgs/Europeen/Tartare_De_Boeuf_Du_Bistrot.jpg",
-        nom : "Tartare De Boeuf Du Bistrot",
-        prix : "10.000 Fcfa",
+        nom: "Tartare De Boeuf Du Bistrot",
+        prix: "10.000 Fcfa",
     },
 ]
 
 
 
-for (const plat of platsEuropeen){
+for (const plat of platsEuropeen) {
     let div1 = document.createElement("div");
     div1.classList.add("col-3");
     divCardEuropeen.appendChild(div1);
@@ -338,7 +352,7 @@ for (const plat of platsEuropeen){
     div1.appendChild(div);
 
     let img = document.createElement("img")
-    img.classList.add("imageCard");         
+    img.classList.add("imageCard");
     img.src = plat.image;
     div.appendChild(img);
 
