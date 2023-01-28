f3294f2020c6446d4c1cf8e41e2295b18c2070b5
Mamadoukernel <kernel0748@gmail.com>
2023-01-28 13:46:40 +0000

    Ajout des fonctions de suppression, Incrementation et decrementation de la quantite des plats et mise a jour du FCFa

--- assets/script/script.js
+++ assets/script/script.js
@@ -43,21 +43,21 @@ const platsAfricain = [
         id:4,
         image: "assets/imgs/africain/im9.jpg",
         nom: "Soupe du Pêcheur",
-        prix: "10.000 Fcfa",
+        prix: "10.000",
     },
 
     {
         id:5,
         image: "assets/imgs/africain/im10.jpg",
         nom: "Sauce Graine",
-        prix: "8000 Fcfa",
+        prix: "8000",
     },
 
     {
         id:5,
         image: "assets/imgs/africain/im11.jpg",
         nom: "Garba",
-        prix: "3000 Fcfa",
+        prix: "3000",
     },
 ]
 
@@ -86,7 +86,7 @@ for (const plat of platsAfricain) {
     let prixPlat = document.createElement("p");
     prixPlat.classList.add("priPlat");
     div.appendChild(prixPlat);
-    prixPlat.textContent = plat.prix;
+    prixPlat.textContent = plat.prix + " FCFA"
 
     let ButtonPlat = document.createElement("button");
     ButtonPlat.classList.add("buttonPlat");
@@ -116,12 +116,12 @@ for (const plat of platsAfricain) {
         tr.appendChild(td3);
 
         let td4 = document.createElement("td");
-        td4.textContent = produit.prix;
+        td4.textContent = produit.prix+" FCFA";
         td4.setAttribute("id","produit-prix-" + produit.id);
         tr.appendChild(td4);
 
         let td5 = document.createElement("td");
-        td5.textContent = produit.prix;
+        td5.textContent = produit.prix+ " FCFA";
         td5.setAttribute("id","produit-total-" + produit.id);
         tr.appendChild(td5);
 
@@ -157,49 +157,49 @@ const platsAmericain = [
         id:6,
         image: "assets/imgs/americain/Cauliflower.jpg",
         nom: "Cauliflower",
-        prix: "7000 Fcfa",
+        prix: "7000",
     },
 
     {
         id:7,
         image: "assets/imgs/americain/easy_all_americaine.jpg",
         nom: "Easy all ameriaine",
-        prix: "8000 Fcfa",
+        prix: "8000",
     },
 
     {
         id:8,
         image: "assets/imgs/americain/quesadillas_au_poulet.jpg",
         nom: "Quesadillas au poulet",
-        prix: "8000 Fcfa",
+        prix: "8000",
     },
 
     {
         id:9,
         image: "assets/imgs/americain/roasted_chilli.jpg",
         nom: "Soupe de mouton",
-        prix: "7000 Fcfa",
+        prix: "7000",
     },
 
     {
         id:10,
         image: "assets/imgs/americain/Salade à l'americaine.jpg",
         nom: "Salade à l'americaine",
-        prix: "5000 Fcfa",
+        prix: "5000",
     },
 
     {
         id:11,
         image: "assets/imgs/americain/super_bowl.jpg",
         nom: "Super bowl",
-        prix: "8000 Fcfa",
+        prix: "8000",
     },
 
     {
         id:12,
         image: "assets/imgs/americain/Travers_de_porc.jpg",
         nom: "Travers de porc",
-        prix: "10.000 Fcfa",
+        prix: "10.000",
     },
 ]
 
@@ -228,7 +228,7 @@ for (const plat of platsAmericain) {
     let prixPlat = document.createElement("p");
     prixPlat.classList.add("priPlat");
     div.appendChild(prixPlat);
-    prixPlat.textContent = plat.prix;
+    prixPlat.textContent = plat.prix+" FCFA";
 
     let ButtonPlat = document.createElement("button");
     ButtonPlat.classList.add("buttonPlat");
@@ -258,12 +258,12 @@ for (const plat of platsAmericain) {
         tr.appendChild(td3);
 
         let td4 = document.createElement("td");
-        td4.textContent = produit.prix;
+        td4.textContent = produit.prix+" FCFA";
         td4.setAttribute("id","produit-prix-" + produit.id);
         tr.appendChild(td4);
 
         let td5 = document.createElement("td");
-        td5.textContent = produit.prix;
+        td5.textContent = produit.prix+" FCFA";
         td5.setAttribute("id","produit-total-" + produit.id);
         tr.appendChild(td5);
 
@@ -300,49 +300,49 @@ const platsAsiatique = [
         id:13,
         image: "assets/imgs/Asiatique/Boeuf_lok_lak.jpg",
         nom: "Boeuf lok lak",
-        prix: "7000 Fcfa",
+        prix: "7000",
     },
 
     {
         id:14,
         image: "assets/imgs/Asiatique/recette_de_porc_japonnaise.jpg",
         nom: "Recette de porc japonnaise",
-        prix: "8000 Fcfa",
+        prix: "8000",
     },
 
     {
         id:15,
         image: "assets/imgs/Asiatique/rice_noodle_soup.jpg",
         nom: "Rice noodle soup",
-        prix: "8000 Fcfa",
+        prix: "8000",
     },
 
     {
         id:16,
         image: "assets/imgs/Asiatique/nems.jpg",
         nom: "Nems",
-        prix: "7000 Fcfa",
+        prix: "7000",
     },
 
     {
         id:17,
         image: "assets/imgs/Asiatique/sczechuan.jpg",
         nom: "Sczechuan",
-        prix: "5000 Fcfa",
+        prix: "5000",
     },
 
     {
         id:18,
         image: "assets/imgs/Asiatique/shrimp.jpg",
         nom: "Shrimp",
-        prix: "8000 Fcfa",
+        prix: "8000",
     },
 
     {
         id:19,
         image: "assets/imgs/Asiatique/torikatsu.jpg",
         nom: "torikatsu",
-        prix: "10.000 Fcfa",
+        prix: "10.000",
     },
 ]
 
@@ -371,7 +371,7 @@ for (const plat of platsAsiatique) {
     let prixPlat = document.createElement("p");
     prixPlat.classList.add("priPlat");
     div.appendChild(prixPlat);
-    prixPlat.textContent = plat.prix;
+    prixPlat.textContent = plat.prix+" FCFA";
 
     let ButtonPlat = document.createElement("button");
     ButtonPlat.classList.add("buttonPlat");
@@ -401,12 +401,12 @@ for (const plat of platsAsiatique) {
         tr.appendChild(td3);
 
         let td4 = document.createElement("td");
-        td4.textContent = produit.prix;
+        td4.textContent = produit.prix+" FCFA";
         td4.setAttribute("id","produit-prix-" + produit.id);
         tr.appendChild(td4);
 
         let td5 = document.createElement("td");
-        td5.textContent = produit.prix;
+        td5.textContent = produit.prix+" FCFA";
         td5.setAttribute("id","produit-total-" + produit.id);
         tr.appendChild(td5);
 
@@ -442,63 +442,63 @@ const platsEuropeen = [
         id : 20,
         image: "assets/imgs/Europeen/Escalope_de_veau_milannaise.jpg",
         nom: "Escalope de veau milannaise",
-        prix: "7000 Fcfa",
+        prix: "7000",
     },
 
     {
         id:21,
         image: "assets/imgs/Europeen/German_Pot.jpg",
         nom: "German Pot",
-        prix: "8000 Fcfa",
+        prix: "8000",
     },
 
     {   
         id:22,
         image: "assets/imgs/Europeen/Haluski.jpg",
         nom: "Haluski",
-        prix: "8000 Fcfa",
+        prix: "8000",
     },
 
     {
         id:23,
         image: "assets/imgs/Europeen/potato_Round.jpg",
         nom: "Potato Round",
-        prix: "7000 Fcfa",
+        prix: "7000",
     },
 
     {
         id:24,
         image: "assets/imgs/Europeen/poulet_aux_noix_de_cajoux.jpg",
         nom: "poulet aux noix de cajoux",
-        prix: "12.000 Fcfa",
+        prix: "12.000",
     },
 
     {
         id:25,
         image: "assets/imgs/Europeen/Risoto_Royal.jpg",
         nom: "Risoto_Royal",
-        prix: "8000 Fcfa",
+        prix: "8000",
     },
 
     {
         id:26,
         image: "assets/imgs/Europeen/Roti_De_Porc_aux_Pommes_et_pruneaux.jpg",
         nom: "Roti De Porc aux Pommes et pruneaux",
-        prix: "10.000 Fcfa",
+        prix: "10.000",
     },
 
     {
         id:27,
         image: "assets/imgs/Europeen/Roti_De_Porc.jpg",
         nom: "Roti De Porc",
-        prix: "10.000 Fcfa",
+        prix: "10.000",
     },
 
     {
         id:28,
         image: "assets/imgs/Europeen/Tartare_De_Boeuf_Du_Bistrot.jpg",
         nom: "Tartare De Boeuf Du Bistrot",
-        prix: "10.000 Fcfa",
+        prix: "10.000",
     },
 ]
 
@@ -527,7 +527,7 @@ for (const plat of platsEuropeen) {
     let prixPlat = document.createElement("p");
     prixPlat.classList.add("priPlat");
     div.appendChild(prixPlat);
-    prixPlat.textContent = plat.prix;
+    prixPlat.textContent = plat.prix+" FCFA";
 
     let ButtonPlat = document.createElement("button");
     ButtonPlat.classList.add("buttonPlat");
@@ -557,12 +557,12 @@ for (const plat of platsEuropeen) {
         tr.appendChild(td3);
 
         let td4 = document.createElement("td");
-        td4.textContent = produit.prix;
+        td4.textContent = produit.prix+" FCFA";
         td4.setAttribute("id","produit-prix-" + produit.id);
         tr.appendChild(td4);
 
         let td5 = document.createElement("td");
-        td5.textContent = produit.prix;
+        td5.textContent = produit.prix+" FCFA";
         td5.setAttribute("id","produit-total-" + produit.id);
         tr.appendChild(td5);
 
@@ -595,7 +595,7 @@ function AjouterProduit(id){
     let prixProduitT = document.getElementById(`produit-total-${id}`);
     let prixProduitU = document.getElementById(`produit-prix-${id}`);
     let prixproduitTotal = parseInt(prixProduitU.textContent) * qteProduit.textContent
-    prixProduitT.textContent = prixproduitTotal
+    prixProduitT.textContent = prixproduitTotal+" FCFA"
 
 }
 
@@ -603,12 +603,15 @@ function AjouterProduit(id){
 function diminuerProduit(id){
     let qteProduit = document.getElementById(`quantite-total-${id}`);
     let valeurQuantiteProduit = parseInt(qteProduit.textContent) - 1;
-    qteProduit.textContent = valeurQuantiteProduit;
-
+    if(valeurQuantiteProduit >0){
+        qteProduit.textContent = valeurQuantiteProduit;
+    }else{
+        qteProduit.textContent = 1;
+    }
     let prixProduitT = document.getElementById(`produit-total-${id}`);
     let prixProduitU = document.getElementById(`produit-prix-${id}`);
     let prixproduitTotal = parseInt(prixProduitU.textContent) * qteProduit.textContent
-    prixProduitT.textContent = prixproduitTotal
+    prixProduitT.textContent = prixproduitTotal+" FCFA"
     if(qteProduit.textContent == 0){
     }
 
@@ -616,5 +619,13 @@ function diminuerProduit(id){
 
 
 function SupprimerProduit(id){
-    console.log("supression produit", id)
+    let qteProduit = document.getElementById(`quantite-total-${id}`);
+    qteProduit.textContent = 0;
+    let prixProduitT = document.getElementById(`produit-total-${id}`);
+    prixProduitT.textContent = 0;
+    let prixProduitU = document.getElementById(`produit-prix-${id}`);
+    prixProduitU.textContent = 0;
+    let tr = document.getElementById(`bouton-sup-${id}`).closest("tr");
+    tr.remove();
+
 }
